2cd2ebc9229c2af4347844d198bbfb4e5920df90
ramprasadgangarapu <47077986+robertjnr@users.noreply.github.com>
2022-07-22 12:38:36 +0530

    Update cal.groovy

--- src/cal.groovy
+++ src/cal.groovy
@@ -1,9 +1,9 @@
 def mult(name){
    println("HELLLO ${name},how are u?")
 }
-def add(int a,int b){
+def adds(int a,int b){
    println(a+b)
 }
-def sub(int a ,int b){
+def subs(int a ,int b){
   println(a-b)
 }
