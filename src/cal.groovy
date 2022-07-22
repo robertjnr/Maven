d9972650f362a4129c6c1098b7cd64887044620c
ramprasadgangarapu <47077986+robertjnr@users.noreply.github.com>
2022-07-22 12:29:02 +0530

    Update cal.groovy

--- src/cal.groovy
+++ src/cal.groovy
@@ -1,6 +1,9 @@
-def add(a,b){
-  return(a+b)
+def call(name){
+   println("HELLLO ${name},how are u?")
 }
-def sub(a,b){
-  return(a-b)
+def add(int a,int b){
+   println(a+b)
+}
+def sub(int a ,int b){
+  println(a-b)
 }
